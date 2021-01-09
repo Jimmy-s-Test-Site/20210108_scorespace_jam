d24cc9caebc3d663f8bff10a7953ff1ca3dfa5e2
Julian <jaacko.torus@gmail.com>
2021-01-09 13:25:42 -0500

    new layout, finished neighbor dependencies

--- Julian/Level/Test2/Test2.gd
+++ Julian/Level/Test2/Test2.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 export (int) var unit_room_size : int = 20
 
 export (Array, PackedScene) var rooms : Array
@@ -7,23 +8,23 @@ export (Array, PackedScene) var rooms : Array
 onready var Rooms : Node2D = $Rooms
 
 var position_array = [
-	{"size":  7, "area": 3, "y_offset": 5},
-	{"size": 12, "area": 2, "y_offset": 4},
-	{"size":  9, "area": 3, "y_offset": 2},
-	{"size":  8, "area": 4, "y_offset": 0},
-	{"size":  9, "area": 3, "y_offset": 2},
-	{"size":  8, "area": 4, "y_offset": 0},
-	{"size":  9, "area": 3, "y_offset": 2},
-	{"size": 12, "area": 2, "y_offset": 4}
+	{"size": 13, "area": 2, "y_offset": 0},
+	{"size": 12, "area": 2, "y_offset": 1}
 ]
 
 func level_layout_cycle(
+	room_settings : Array,
 	position_array : Array,
-	unit_room_size : int,
 	x_offset : int,
 	start : int = 0,
-	end : int = 8
+	end : int = -1
 ) -> int:
+	var rooms_node = room_settings[0]
+	var rooms_scenes = room_settings[1]
+	var unit_room_size = room_settings[2]
+	
+	if end == -1: end = position_array.size()
+	
 	for column_idx in range(position_array.size()):
 		if start > column_idx: continue
 		if end <= column_idx: break
@@ -33,8 +34,8 @@ func level_layout_cycle(
 		
 		for room_idx in range(column.size):
 			var grid_position = Vector2(column_idx, room_idx)
-			var rand_room_idx = randi() % self.rooms.size()
-			var new_room = self.rooms[rand_room_idx].instance()
+			var rand_room_idx = randi() % rooms_scenes.size()
+			var new_room = rooms_scenes[rand_room_idx].instance()
 			
 			new_room.id = [grid_position.x, grid_position.y]
 			new_room.size = room_scale
@@ -43,24 +44,31 @@ func level_layout_cycle(
 				unit_room_size * column.y_offset + room_scale * room_idx
 			)
 			
-			self.Rooms.add_child(new_room)
+			rooms_node.add_child(new_room)
 		
 		x_offset += unit_room_size * column.area
 	
 	return x_offset
 
 func level_layout(
+	room_settings : Array,
 	position_array : Array,
-	unit_room_size : int,
 	start_x : int,
-	cycle_start : int,
-	n_loops : int,
-	cycle_end : int
+	cycle_settings : Array
 ) -> void:
-	var x_acc = self.level_layout_cycle(position_array, unit_room_size, start_x, cycle_start)
+	var cycle_start : int = cycle_settings[0]
+	var n_loops : int = cycle_settings[1]
+	var cycle_end : int = cycle_settings[2]
+	
+	var x_acc = self.level_layout_cycle(room_settings, position_array, start_x, cycle_start)
 	for n in range(n_loops):
-		x_acc = self.level_layout_cycle(position_array, unit_room_size, x_acc)
-	self.level_layout_cycle(position_array, unit_room_size, x_acc, 0, cycle_end)
+		x_acc = self.level_layout_cycle(room_settings, position_array, x_acc)
+	self.level_layout_cycle(room_settings, position_array, x_acc, 0, cycle_end)
 
 func _ready():
-	self.level_layout(self.position_array, self.unit_room_size, 0, 2, 2, 4)
+	self.level_layout(
+		[self.Rooms, self.rooms, self.unit_room_size],
+		self.position_array,
+		0,
+		[0, 16, 4]
+	)
--- Julian/Rooms/Room.gd
+++ Julian/Rooms/Room.gd
@@ -2,9 +2,33 @@ extends Node2D
 
 export (int) var size : int = 200 setget set_size
 
-var neighbour_network = [
-	[]
-]
+var neighbour_network := {
+	"odd_column" : {
+		"first": [[-1, 0], [ 0, 1], [ 1, 0]],
+		"last" : [[-1,-1], [ 0,-1], [ 1,-1]],
+		"else" : [[-1,-1], [-1, 0], [ 0,-1], [ 0, 1], [ 1,-1], [ 1, 0]]
+	},
+	"even_column" : {
+		"first": [[-1, 0], [-1, 1], [ 0, 1], [ 1, 0], [ 1, 1]],
+		"last" : [[-1, 0], [-1, 1], [ 0,-1], [ 1, 0], [ 1, 1]],
+		"else" : [[-1, 0], [-1, 1], [ 0,-1], [ 0, 1], [ 1, 0], [ 1, 1]]
+	}
+}
+
+var naighbour_to_wall := {
+	"odd_column": {
+		[-1,-1]: "LeftUp",  [-1, 0]: "LeftDown",
+		[ 0,-1]: "Up",      [ 0, 1]: "Down",
+		[ 1,-1]: "RightUp", [ 1, 0]: "RightDown"
+	},
+	"even_column": {
+		[-1, 0]: "LeftUp",  [-1, 1]: "LeftDown",
+		[ 0,-1]: "Up",      [ 0, 1]: "Down",
+		[ 1, 0]: "RightUp", [ 1, 1]: "RightDown"
+	}
+}
+
+var open_neighbour_network := {} # self.neighbour_network only [1,x]
 
 var id : Vector2 = Vector2.ZERO setget set_id
 var neighbours : Array = []
