422316c9cc07bb51eaaacf53fdeb85b4e5f68936
Julian <jaacko.torus@gmail.com>
2021-01-10 18:09:18 -0500

    splash screen sorta works
    gotta click twice on go :(

--- Julian/Level/Level.gd
+++ Julian/Level/Level.gd
@@ -32,9 +32,7 @@ func get_room_positions(length : int, maj_height : int) -> Array:
 	
 	return room_positions
 
-func instantiate_rooms(positions : Array) -> Array:
-	var room_instances := []
-	
+func instantiate_rooms(positions : Array) -> void:
 	for room_pos in positions:
 		var rand_room_idx = randi() % self.rooms.size()
 		var new_room = self.rooms[rand_room_idx].instance()
@@ -54,33 +52,39 @@ func instantiate_rooms(positions : Array) -> Array:
 		
 		if room_pos[0] == 0:
 			new_room.get_node("Walls/LeftUp").disabled = false
-			new_room.get_node("Walls/LeftDown").disabled = false
-			new_room.get_node("Walls/RightUp").disabled = false
-			new_room.get_node("Walls/RightDown").disabled = false
 			new_room.get_node("Walls/LeftUp").visible = true
+			new_room.get_node("Walls/LeftDown").disabled = false
 			new_room.get_node("Walls/LeftDown").visible = true
+		elif room_pos[0] == 11 * unit_room_size:
+			new_room.get_node("Walls/RightUp").disabled = false
 			new_room.get_node("Walls/RightUp").visible = true
+			new_room.get_node("Walls/RightDown").disabled = false
 			new_room.get_node("Walls/RightDown").visible = true
 		
 		if room_pos[1] == 0:
 			new_room.get_node("Walls/Up").disabled = false
 			new_room.get_node("Walls/Up").visible = true
+			new_room.get_node("Walls/LeftUp").disabled = false
+			new_room.get_node("Walls/LeftUp").visible = true
+			new_room.get_node("Walls/RightUp").disabled = false
+			new_room.get_node("Walls/RightUp").visible = true
 		
-		if is_even(room_pos[0]) and room_pos[1] == 17 * unit_room_size:
+		if (
+			(    is_even(room_pos[0]) and room_pos[1] == 6 * unit_room_size) or
+			(not is_even(room_pos[0]) and room_pos[1] == 7 * unit_room_size)
+		):
 			new_room.get_node("Walls/Down").disabled = false
 			new_room.get_node("Walls/Down").visible = true
-		elif not is_even(room_pos[0]) and room_pos[1] == 16 * unit_room_size:
-			new_room.get_node("Walls/Down").disabled = false
-			new_room.get_node("Walls/Down").visible = true
+			new_room.get_node("Walls/LeftDown").disabled = false
+			new_room.get_node("Walls/LeftDown").visible = true
+			new_room.get_node("Walls/RightDown").disabled = false
+			new_room.get_node("Walls/RightDown").visible = true
 		
 		self.Rooms.add_child(new_room)
-		room_instances.append(new_room)
-	
-	return room_instances
 
-func generate() -> Array:
-	var positions = self.get_room_positions(26, 18)
-	return self.instantiate_rooms(positions)
+func generate() -> void:
+	var positions = self.get_room_positions(12, 8)
+	self.instantiate_rooms(positions)
 
 func _ready() -> void:
 	pass
