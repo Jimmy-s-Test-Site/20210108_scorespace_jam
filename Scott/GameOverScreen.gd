8e70f05c50a674fdb04169a0acb5f75fc8513264
Julian <jaacko.torus@gmail.com>
2021-01-10 18:31:28 -0500

    integrated gameoverscreen into main scene

--- Scott/GameOverScreen.gd
+++ Scott/GameOverScreen.gd
@@ -1,20 +1,23 @@
 extends Control
 
+signal proceed
+
+signal fruit_score_updated
+signal time_score_updated
+
 func _ready():
 	pass
 
 func _on_Restart_pressed():
-	pass # Replace with function body.
-signal fruit_score_updated
-signal time_score_updated
+	emit_signal("proceed")
 
 var fruit_score: = 0 setget set_fruit_score
 var time_score: = 0 setget set_time_score
 
 func set_time_score(value: int) -> void:
 	time_score = value
-	emit_signal("time_score_updated")
+	#emit_signal("time_score_updated")
 
 func set_fruit_score(value: int) -> void:
 	fruit_score = value
-	emit_signal("fruit_score_updated")
+	#emit_signal("fruit_score_updated")
