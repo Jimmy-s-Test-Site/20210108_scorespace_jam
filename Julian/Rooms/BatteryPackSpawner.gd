78fe6071d7c8cf45706b19371fc3b6f72a574846
Justin <justp02@hotmail.com>
2021-01-11 02:28:43 -0600

    Fruits and BatteryPack Spawning Added

--- Julian/Rooms/BatteryPackSpawner.gd
+++ Julian/Rooms/BatteryPackSpawner.gd
@@ -1,26 +1,24 @@
 extends Node2D
 
-export (float) var percent_packNRG : float = .75
-
+export (float) var percent_packNRG : float = .40
 export (PackedScene) var battery_pack
 
 var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-func random_spawn_position():
-	rng.randomize()
-		#will get a random number b/w room width
-	var random_x = rng.randi_range(0,$Sprite.texture.get_width())
-		#will get a random number b/w room height
-	var random_y = rng.randi_range(0,$Sprite.texture.get_height())
+	bPack_spawn()
 
 func bPack_spawn():
 	rng.randomize()
 	
+	var newRandPos = Vector2(
+		clamp(rng.randf()*1000,125,875),
+		clamp(rng.randf()*1000,125,875)
+		)
+
 	if rng.randf() < percent_packNRG: #less than this then spawn
-		add_child(battery_pack)
-		battery_pack.position = Vector2(random_spawn_position(),random_spawn_position()) #will spawn fruit at random x&y within room
-	
+		var new_battery_pack = battery_pack.instance()
+		new_battery_pack.position = newRandPos
+		add_child(new_battery_pack)
+
--- Julian/Rooms/FruitSpawner.gd
+++ Julian/Rooms/FruitSpawner.gd
@@ -1,26 +1,25 @@
 extends Node2D
 
-export (float) var percent_froot : float = .75
+export (float) var percent_froot : float = 0.70
 export (Array, PackedScene) var fruits
 
 var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-func random_spawn_position():
-	rng.randomize()
-		#will get a random number b/w room width
-	var random_x = rng.randi_range(0,$Sprite.texture.get_width())
-		#will get a random number b/w room height
-	var random_y = rng.randi_range(0,$Sprite.texture.get_height())
+	froot_spawn()
 
 func froot_spawn():
-	rng.randomize()
-	
-	if rng.randf() < percent_froot: #less than this then spawn
-		var froot = fruits[rng.randi_range(0,4)].instance()
-		add_child(froot)
-		froot.position = Vector2(random_spawn_position(),random_spawn_position()) #will spawn fruit at random x&y within room
-	
+	for fruit in fruits:
+		rng.randomize()
+		
+		var newRandPos = Vector2(
+			clamp(rng.randf()*1000,125,875),
+			clamp(rng.randf()*1000,125,875)
+			)
+
+		if rng.randf() < percent_froot: #less than this then spawn
+			var new_froot = fruit.instance()
+			new_froot.position = newRandPos
+			add_child(new_froot)
+
