9c147712218241a969a44201f667572693054bdb
Justin <justp02@hotmail.com>
2021-01-10 20:25:05 -0600

    Added RNG Froot Spawn to Room
    My attempt at RNG spawning froot randomly in a room

--- Julian/Rooms/Room.gd
+++ Julian/Rooms/Room.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
 export (int) var size : int = 200 setget set_size
+export (int) var percent_froot : int = 100
+export (int) var percent_minion : int = 100
+
+export (NodePath) var HairFrootNodePath := "res://Justin/Items/Hair_Froot.tscn"
+export (NodePath) var BlavocadoFrootNodePath := "res://Justin/Items/Blavocado_Froot.tscn"
+export (NodePath) var IdekFrootNodePath := "res://Justin/Items/Idek_Froot.tscn"
+export (NodePath) var SchmorFrootNodePath := "res://Justin/Items/Schmor_Froot.tscn"
 
 var id : Array = [0, 0] setget set_id
+var rng = RandomNumberGenerator.new()
 
 func set_size(value : int) -> void:
 	size = value
@@ -23,3 +31,37 @@ func set_id(value : Array) -> void:
 
 func _ready() -> void:
 	$Sprite.visible = true
+
+func random_spawn_position():
+	rng.randomize()
+		#will get a random number b/w room width
+	var random_x = rng.randi_range(0,$Sprite.texture.get_width())
+		#will get a random number b/w room height
+	var random_y = rng.randi_range(0,$Sprite.texture.get_height())
+
+func froot_spawn():
+	rng.randomize()
+	
+	var chanceSpawn = rng.randi_range(0,100) # get a percent chance of spawn
+	
+	if chanceSpawn>=percent_froot:
+		var frootchoice = rng.randi_range(1,4)
+		var frootpath = SchmorFrootNodePath #default is SChmorfFroot
+		
+		if frootchoice == 1: #HairFroot will be spawned
+			frootpath = HairFrootNodePath
+		elif frootchoice == 2: #BlavocadoFroot will be spawned
+			frootpath = BlavocadoFrootNodePath
+		elif frootchoice == 3: #IdekFroot will be spawned
+			frootpath = IdekFrootNodePath
+		elif frootchoice == 4: #SchmorFroot will be spawned (DEFAULT)
+			frootpath = SchmorFrootNodePath 
+
+		var froot = frootpath.instance()
+		froot.position = Vector2(random_spawn_position(),random_spawn_position()) #will spawn fruit at random x&y within room
+	
+	#func shoot():
+	#projectile node path  EX:load("res://Justin/Enemies/Boss/projectile.tscn")
+	#var projectile = load(str(projectileNodePath))
+	#var bullet = projectile.instance()
+	
